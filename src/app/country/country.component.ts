70c9f11a4be35c85e172dbd7793dfc0957040d27
Michał Pawlak <michal.pawlak001@gmail.com>
2024-05-31 21:30:43 +0200

    forgot to add new files

---
+++ src/app/country/country.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country',
+  standalone: true,
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.css']
+})
+export class CountryComponent {
+
+}
