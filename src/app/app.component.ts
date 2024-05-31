03cd625f8010b06d3dc73979bd67b10aae4bb631
Michał Pawlak <michal.pawlak001@gmail.com>
2024-05-31 21:30:26 +0200

    made some progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+import { HeaderComponent } from "./header/header.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { CountryComponent } from "./country/country.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavigationComponent,CountryComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   imports: [
     AppComponent
   ],
   providers: [],
+  declarations: [
+    NavigationComponent,
+    CountryComponent
+  ],
 })
 export class AppModule { }
