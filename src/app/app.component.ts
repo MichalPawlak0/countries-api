392f80968cf5cb78cc789179d04c010cbcfeca36
Michał Pawlak <michal.pawlak001@gmail.com>
2024-06-09 13:15:38 +0200

    changing displayed countries by regions and search query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,17 @@
 import { Component, computed, input } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { NavigationComponent } from "./navigation/navigation.component";
-import { CountryComponent } from "./country/country.component";
+import { HeaderComponent } from './header/header.component';
 
-import COUNTRIES from "../../data.json";
+import { CountriesComponent } from './countries/countries.component';
 
 //let selectedRegionElVal = (<HTMLSelectElement>document.getElementById("regionSelection"))?.value
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavigationComponent, CountryComponent],
+  imports: [HeaderComponent, CountriesComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'countries-api';
-  selectedRegion = "Asia";
-
-  allRegions = computed(()=>{return COUNTRIES.
-    map((country)=>{return country.region})
-    .filter((val, ind, arr)=>{
-      return ind === arr.indexOf(val);
-    })
-})
-
-filteredCountries = computed(()=>{
-  return COUNTRIES.filter((country)=>{
-    return country.region === this.selectedRegion;
-  })
-})
-
-onCountryChange(selRegion:string){
-  let selectedRegionElVal = (<HTMLSelectElement>document.getElementById("regionSelection")).value
-  this.selectedRegion = selectedRegionElVal;
-  this.filteredCountries = computed(()=>{
-  return COUNTRIES.filter((country)=>{
-    return country.region === this.selectedRegion;
-  })
-})
-}
-
 }
